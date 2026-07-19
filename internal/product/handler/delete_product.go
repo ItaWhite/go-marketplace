2cb8ce4156e4661fee489074efaebc6e93da3f40
ItaWhite <65124393+ItaWhite@users.noreply.github.com>
2026-07-20 00:47:08 +0300

    fix(handler): return 404 instead of 400 for non-existent id

--- internal/product/handler/delete_product.go
+++ internal/product/handler/delete_product.go
@@ -16,17 +16,24 @@ func (h *ProductHandler) DeleteProduct(w http.ResponseWriter, r *http.Request) {
 		switch {
 		case errors.Is(err, productfeat.ErrInvalidID):
 			http.Error(w, "invalid product id", http.StatusBadRequest)
-		case errors.Is(err, productfeat.ErrNotFound):
-			http.Error(w, "product not found", http.StatusNotFound)
 		default:
 			http.Error(w, "internal error", http.StatusInternalServerError)
 		}
 		return
 	}
+
 	err = h.service.DeleteProduct(r.Context(), id)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
 		slog.Error("DeleteProductHandler", "error", err)
+
+		switch {
+		case errors.Is(err, productfeat.ErrInvalidID):
+			http.Error(w, "invalid product id", http.StatusBadRequest)
+		case errors.Is(err, productfeat.ErrNotFound):
+			http.Error(w, "product not found", http.StatusNotFound)
+		default:
+			http.Error(w, "internal error", http.StatusInternalServerError)
+		}
 		return
 	}
 	w.WriteHeader(http.StatusNoContent)
