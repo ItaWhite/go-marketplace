74f23c0a1372d6bf3dd9f5ae7e5000a569d923d6
ItaWhite <65124393+ItaWhite@users.noreply.github.com>
2026-07-20 19:57:02 +0300

    feat(repository): add query parameters support for get products repository

--- internal/product/repository/get_products.go
+++ internal/product/repository/get_products.go
@@ -2,23 +2,57 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"go-marketplace/internal/core/domain"
 )
 
 func (r *productRepository) GetProducts(ctx context.Context, limit, offset int) ([]domain.Product, error) {
-	rows, err := r.db.Query(ctx, "select id, version, name, price, created_at  from products;")
+	query := "select id, version, name, price, created_at  from products order by id"
+
+	var args []any
+
+	if limit != 0 {
+		args = append(args, limit)
+		query += fmt.Sprintf(" limit $%d", len(args))
+	}
+	if offset != 0 {
+		args = append(args, offset)
+		query += fmt.Sprintf(" offset $%d", len(args))
+	}
+
+	rows, err := r.db.Query(ctx, query, args...)
 	if err != nil {
-		return []domain.Product{}, err
+		return []domain.Product{}, fmt.Errorf("error select: %w", err)
 	}
 	defer rows.Close()
-	var productList []domain.Product
+
+	var productList []ProductModel
+
 	for rows.Next() {
-		var product domain.Product
+		var product ProductModel
+
 		err = rows.Scan(&product.ID, &product.Version, &product.Name, &product.Price, &product.CreatedAt)
 		if err != nil {
-			return productList, err
+			return nil, fmt.Errorf("error scan: %w", err)
 		}
+
 		productList = append(productList, product)
 	}
-	return productList, nil
+	if rows.Err() != nil {
+		return nil, fmt.Errorf("rows next error: %w", err)
+	}
+
+	productDomainsList := make([]domain.Product, len(productList))
+
+	for i, m := range productList {
+		productDomainsList[i] = domain.Product{
+			ID:        m.ID,
+			Version:   m.Version,
+			Name:      m.Name,
+			Price:     m.Price,
+			CreatedAt: m.CreatedAt,
+		}
+	}
+
+	return productDomainsList, nil
 }
