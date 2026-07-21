62ac96b98bb29ce622c94b0bb398a1a65a1b56d6
ItaWhite <65124393+ItaWhite@users.noreply.github.com>
2026-07-21 19:50:02 +0300

    refactor(service): wrap error from repository

--- internal/product/service/get_product.go
+++ internal/product/service/get_product.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"go-marketplace/internal/core/domain"
 	productfeat "go-marketplace/internal/product"
 )
@@ -10,5 +11,10 @@ func (s *ProductService) GetProduct(ctx context.Context, id int) (domain.Product
 	if id <= 0 {
 		return domain.Product{}, productfeat.ErrInvalidID
 	}
-	return s.repo.GetProduct(ctx, id)
+	product, err := s.repo.GetProduct(ctx, id)
+	if err != nil {
+		return domain.Product{}, fmt.Errorf("get product: %w", err)
+	}
+
+	return product, nil
 }
