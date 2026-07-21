6b1ca01c66998f6df9769c27241f0d9e5e9f1210
ItaWhite <65124393+ItaWhite@users.noreply.github.com>
2026-07-21 19:58:01 +0300

    feat(repository): map product model to domain in get product method

--- internal/product/repository/get_product.go
+++ internal/product/repository/get_product.go
@@ -3,23 +3,34 @@ package repository
 import (
 	"context"
 	"errors"
+	"fmt"
 	"go-marketplace/internal/core/domain"
-	product2 "go-marketplace/internal/product"
+	productfeat "go-marketplace/internal/product"
 
 	"github.com/jackc/pgx/v5"
 )
 
 func (r *productRepository) GetProduct(ctx context.Context, id int) (domain.Product, error) {
-	var product domain.Product
-	err := r.db.QueryRow(ctx, "select id, version, name, price, created_at from products where id = $1", id).
-		Scan(&product.ID, &product.Version, &product.Name, &product.Price, &product.CreatedAt)
+	row := r.db.QueryRow(ctx, "select id, version, name, price, created_at from products where id = $1", id)
+
+	var product ProductModel
+
+	err := row.Scan(&product.ID, &product.Version, &product.Name, &product.Price, &product.CreatedAt)
 	if err != nil {
 		switch {
 		case errors.Is(err, pgx.ErrNoRows):
-			return domain.Product{}, product2.ErrNotFound
-		default:
-			return domain.Product{}, err
+			return domain.Product{}, fmt.Errorf("user with id=%d not found: %w", id, productfeat.ErrNotFound)
 		}
+		return domain.Product{}, err
 	}
-	return product, nil
+
+	productDomain := domain.Product{
+		ID:        product.ID,
+		Version:   product.Version,
+		Name:      product.Name,
+		Price:     product.Price,
+		CreatedAt: product.CreatedAt,
+	}
+
+	return productDomain, nil
 }
