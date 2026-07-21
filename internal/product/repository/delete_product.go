8ded5899a8dede4a7e3a812d08da4514e6135158
ItaWhite <65124393+ItaWhite@users.noreply.github.com>
2026-07-21 22:33:01 +0300

    refactor(repository): warp returning errors

--- internal/product/repository/delete_product.go
+++ internal/product/repository/delete_product.go
@@ -2,16 +2,18 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"go-marketplace/internal/core/transport/errors"
 )
 
 func (r *productRepository) DeleteProduct(ctx context.Context, id int) error {
 	cmd, err := r.db.Exec(ctx, "delete from products where id = $1", id)
 	if err != nil {
-		return err
+		return fmt.Errorf("exec sql query: %w", err)
 	}
 	if cmd.RowsAffected() == 0 {
-		return core_errors.ErrNotFound
+		return fmt.Errorf("user with id=%d not found: %w", id, core_errors.ErrNotFound)
 	}
+
 	return nil
 }
