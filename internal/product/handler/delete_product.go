9e306fe182cd97c81a5c6c8bd1d48c1a9b13aaf7
ItaWhite <65124393+ItaWhite@users.noreply.github.com>
2026-07-22 23:42:08 +0300

    refactor(handler): fix typo in logging function

--- internal/product/handler/delete_product.go
+++ internal/product/handler/delete_product.go
@@ -26,7 +26,7 @@ func (h *ProductHandler) DeleteProduct(w http.ResponseWriter, r *http.Request) {
 			slog.Warn("product not found", "error", err)
 			http.Error(w, "product not found", http.StatusNotFound)
 		default:
-			slog.Error("get product", "error", err)
+			slog.Error("delete product", "error", err)
 			http.Error(w, "internal error", http.StatusInternalServerError)
 		}
 		return
