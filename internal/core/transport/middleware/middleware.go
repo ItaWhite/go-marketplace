dcb5da9fe837e2c44a4250c4ffecb9abdb194ce7
ItaWhite <65124393+ItaWhite@users.noreply.github.com>
2026-07-24 16:36:27 +0300

    fix: fix typo in security header value

--- internal/core/transport/middleware/middleware.go
+++ internal/core/transport/middleware/middleware.go
@@ -11,7 +11,7 @@ func SecurityHeaders(next http.Handler) http.Handler {
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("Strict-Transport-Security", "max-age=63072000;includeSubDomains;preload")
-		w.Header().Set("Content-Security-Policy", "default-srс 'self'")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'")
 		w.Header().Set("Referrer-Policy", "no-referrer")
 		next.ServeHTTP(w, r)
 	})
