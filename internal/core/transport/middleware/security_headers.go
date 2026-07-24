92b5c2f43247b058a28c29aee4da172916d1bc98
ItaWhite <65124393+ItaWhite@users.noreply.github.com>
2026-07-24 16:38:02 +0300

    refactor: split middleware file

--- internal/core/transport/middleware/security_headers.go
+++ internal/core/transport/middleware/security_headers.go
@@ -1,9 +1,6 @@
 package middleware
 
-import (
-	"log/slog"
-	"net/http"
-)
+import "net/http"
 
 func SecurityHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -13,13 +10,6 @@ func SecurityHeaders(next http.Handler) http.Handler {
 		w.Header().Set("Strict-Transport-Security", "max-age=63072000;includeSubDomains;preload")
 		w.Header().Set("Content-Security-Policy", "default-src 'self'")
 		w.Header().Set("Referrer-Policy", "no-referrer")
-		next.ServeHTTP(w, r)
-	})
-}
-
-func Logging(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		slog.Info("HTTP Request", "Method", r.Method, "Path", r.URL, "IP", r.RemoteAddr)
 		next.ServeHTTP(w, r)
 	})
 }
