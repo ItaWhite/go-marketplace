1a6c184fb0e19dfdc6eed7b1c2c757be29cfc8c2
ItaWhite <65124393+ItaWhite@users.noreply.github.com>
2026-07-30 15:31:09 +0300

    feat: add user domain entity

---
+++ internal/core/domain/user.go
@@ -0,0 +1,28 @@
+package domain
+
+import "time"
+
+type UserRole string
+
+const (
+	UserBuyer  UserRole = "buyer"
+	UserSeller UserRole = "seller"
+)
+
+type User struct {
+	ID        int       `json:"id"`
+	Version   int64     `json:"version"`
+	Name      string    `json:"name"`
+	Phone     *string   `json:"phone"`
+	Role      UserRole  `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func (r UserRole) IsValid() bool {
+	switch r {
+	case UserBuyer, UserSeller:
+		return true
+	default:
+		return false
+	}
+}
