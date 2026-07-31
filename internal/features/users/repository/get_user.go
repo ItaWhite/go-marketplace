fe1d63c6fdd63370584b71d079a6b7945f3a94cb
ItaWhite <65124393+ItaWhite@users.noreply.github.com>
2026-07-31 13:24:51 +0300

    feat: implement get user by id

---
+++ internal/features/users/repository/get_user.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"go-marketplace/internal/core/domain"
+	"go-marketplace/internal/core/transport/errors"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func (r *userRepository) GetUser(ctx context.Context, id int) (domain.User, error) {
+	row := r.db.QueryRow(ctx, "select id, version, name, phone, role, created_at from users where id = $1", id)
+
+	var userModel UserModel
+
+	err := row.Scan(&userModel.ID, &userModel.Version, &userModel.Name, &userModel.Phone, &userModel.Role, &userModel.CreatedAt)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return domain.User{}, fmt.Errorf("user with id=%d not found: %w", id, core_errors.ErrNotFound)
+		}
+		return domain.User{}, fmt.Errorf("get user query: %w", err)
+	}
+
+	userDomain := domain.User{
+		ID:        userModel.ID,
+		Version:   userModel.Version,
+		Name:      userModel.Name,
+		Phone:     userModel.Phone,
+		Role:      domain.UserRole(userModel.Role),
+		CreatedAt: userModel.CreatedAt,
+	}
+
+	return userDomain, nil
+}
---
+++ internal/features/users/service/get_user.go
@@ -0,0 +1,21 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"go-marketplace/internal/core/domain"
+	"go-marketplace/internal/core/transport/errors"
+)
+
+func (s *UserService) GetUser(ctx context.Context, id int) (domain.User, error) {
+	if id <= 0 {
+		return domain.User{}, fmt.Errorf("invalid id=%d: %w", id, core_errors.ErrInvalidID)
+	}
+
+	user, err := s.repo.GetUser(ctx, id)
+	if err != nil {
+		return domain.User{}, fmt.Errorf("get user: %w", err)
+	}
+
+	return user, nil
+}
