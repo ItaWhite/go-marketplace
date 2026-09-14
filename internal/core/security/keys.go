f06a1578a74d1d6ad2fdd8e9c6fb133263372eea
ItaWhite <65124393+ItaWhite@users.noreply.github.com>
2026-09-14 12:47:07 +0300

    feat: add public key parsing func

--- internal/core/security/keys.go
+++ internal/core/security/keys.go
@@ -8,6 +8,30 @@ import (
 	"os"
 )
 
+func LoadPublicKey(path string) (*rsa.PublicKey, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(data)
+	if block == nil {
+		return nil, errors.New("failed to decode PEM")
+	}
+
+	key, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	rsaKey, ok := key.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("private key is not RSA")
+	}
+
+	return rsaKey, nil
+}
+
 func LoadPrivateKey(path string) (*rsa.PrivateKey, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
